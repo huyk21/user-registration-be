724fb9edf050f0e327286b183ee247208ca2fda7
Dinh Huy <ldhuy21@clc.fitus.edu.vn>
2024-11-05 19:13:56 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://unspalsh-gallery-lrf9rpdi9-huys-projects-b0a5a2b0.vercel.app/'], // Replace with your frontend URL
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+    credentials: true, // Enable if you need to send cookies
+  });
   
 
   await app.listen(process.env.PORT ?? 3000);
